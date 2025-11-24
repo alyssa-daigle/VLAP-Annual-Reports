51b7dd0bf67d687ddd6d73619b62e70ff5f92ab1
Alyssa Daigle <alyssa.daigle@unh.edu>
2025-11-24 12:24:35 -0500

    solved issue of CYA tables for multiple lakes with same name

--- R/08_CYA_table.R
+++ R/08_CYA_table.R
@@ -3,7 +3,7 @@ make_CYA_table <- function(CYA, LAKEMAP, table_path, input_path) {
     dir.create(table_path, recursive = TRUE)
   }
 
-  # Join with LAKEMAP instead of lookup_table
+  # Join with LAKEMAP to get updated lake names
   CYA_updated <- CYA |>
     left_join(
       LAKEMAP |> select(STATIONID, lake = RELLAKE),
@@ -12,22 +12,31 @@ make_CYA_table <- function(CYA, LAKEMAP, table_path, input_path) {
     mutate(RELLAKE = ifelse(!is.na(lake), lake, RELLAKE)) |>
     select(-lake)
 
-  # Export CSVs for each lake found in LAKEMAP
-  unique_lakes <- unique(LAKEMAP$RELLAKE)
-  for (lake in unique_lakes) {
-    lake_data <- CYA_updated |> filter(RELLAKE == lake)
+  # Export tables grouped by BOTH lake and town
+  lake_town_pairs <- CYA_updated |> distinct(RELLAKE, TOWN)
+
+  for (i in seq_len(nrow(lake_town_pairs))) {
+    lake <- lake_town_pairs$RELLAKE[i]
+    town <- lake_town_pairs$TOWN[i]
+
+    lake_data <- CYA_updated |>
+      filter(RELLAKE == lake, TOWN == town)
 
     # Remove RELLAKE, STATIONID, and TOWN before export
     lake_data_out <- lake_data |> select(-RELLAKE, -STATIONID, -TOWN)
 
-    file_name <- paste0(gsub(" ", "_", lake), "_CYA.csv")
+    # Clean file name: Lake_Town_CYA.csv
+    lake_clean <- gsub(" ", "_", lake)
+    town_clean <- gsub(" ", "_", town)
+    file_name <- paste0(lake_clean, "_", town_clean, "_CYA.csv")
+
     write_csv(lake_data_out, file.path(table_path, file_name))
   }
 
   message(
     "CYA tables exported for ",
-    length(unique_lakes),
-    " lakes to: ",
+    nrow(lake_town_pairs),
+    " lake/town combinations to: ",
     table_path
   )
 }
